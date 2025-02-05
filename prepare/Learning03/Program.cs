c7ecc5335352aa0ba7c4f5168833a0edb3f6eedc
JamKnight1 <jameswise.jk@gmail.com>
2025-02-05 12:17:29 -0700

    Created the Fraction Class and finished the program

--- prepare/Learning03/Program.cs
+++ prepare/Learning03/Program.cs
@@ -4,6 +4,23 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Learning03 World!");
+        // Create a new Fraction object
+        Fraction fraction = new Fraction();
+
+        // Get the top and bottom numbers from the user
+        Console.WriteLine("Give a top number: ");
+        int top = int.Parse(Console.ReadLine());
+        Console.WriteLine("Give a bottom number: ");
+        int bottom = int.Parse(Console.ReadLine());
+
+        // Set the top and bottom numbers in the Fraction object
+        fraction.SetTop(top);
+        fraction.SetBottom(bottom);
+
+        // Display the fraction as a string
+        Console.WriteLine(fraction.GetFractionString());
+
+        // Display the fraction as a decimal
+        Console.WriteLine(fraction.GetDecimalValue());
     }
 }
