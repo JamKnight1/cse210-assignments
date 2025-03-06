6e462093caf975442200bf98009f1966fbd4e27a
JamKnight1 <jameswise.jk@gmail.com>
2025-03-06 13:21:09 -0700

    Created the Shapes program

--- prepare/Learning05/Program.cs
+++ prepare/Learning05/Program.cs
@@ -1,9 +1,29 @@
 using System;
+using System.Globalization;
 
 class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Learning05 World!");
+        List<Shape> shapes = new List<Shape>();
+
+        Square s1 = new Square("Blue", 6);
+        shapes.Add(s1);
+
+        Rectangle s2 = new Rectangle(4, 3, "Purple");
+        shapes.Add(s2);
+
+        Circle s3 = new Circle(12, "Brown");
+        shapes.Add(s3);
+
+        foreach (Shape s in shapes)
+        {
+            string color = s.GetColor();
+
+            double area = s.GetArea();
+
+            Console.WriteLine($"The {color} shape has an area of {area}.");
+        }
+        
     }
 }
